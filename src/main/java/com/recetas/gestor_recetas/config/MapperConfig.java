75a3a0d944200d0e293ea037e54d1f609553de5d
erick <erickjo10x@gmail.com>
2025-06-19 21:53:16 +0200

    add security config

---
+++ src/main/java/com/recetas/gestor_recetas/config/MapperConfig.java
@@ -0,0 +1,19 @@
+package com.recetas.gestor_recetas.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MapperConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT)
+                .setSkipNullEnabled(true)
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE);
+        return modelMapper;
+    }
+}
