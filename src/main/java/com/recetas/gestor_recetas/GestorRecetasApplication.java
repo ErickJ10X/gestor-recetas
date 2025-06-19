b1a66b55dec2fd58cfdf53167b18ff29622c323f
erick <erickjo10x@gmail.com>
2025-06-19 13:01:27 +0200

    Initial commit

---
+++ src/main/java/com/recetas/gestor_recetas/GestorRecetasApplication.java
@@ -0,0 +1,13 @@
+package com.recetas.gestor_recetas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorRecetasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorRecetasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recetas/gestor_recetas/GestorRecetasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recetas.gestor_recetas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorRecetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
